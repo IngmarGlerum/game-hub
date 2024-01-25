244f86e20e979bfa94b305f9c933c5cba5d8981d
Ingmar Glerum <ingmar.glerum@kvk.nl>
2024-01-25 15:57:26 +0100

    displaying platform icons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "ba9899145d544241ab8e0ab4625775ff",
+    key: "dc80903f2bdd41c186464537cbfea959",
   },
 });
