a1c1650da1f809cc1caf02c99aef46e81465ecbe
Ingmar Glerum <ingmar.glerum@kvk.nl>
2024-01-26 14:36:30 +0100

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,16 +3,19 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import { getCroppedImageUrl } from "../services/image-url";
 
 interface Props {
   game: Game;
 }
 
+//https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+
 const GameCard = ({ game }: Props) => {
   // zonder overflow hidden plaatje groter dan container, borderradius niet zichtbaar
   return (
     <Card borderRadius={"10px"} overflow={"hidden"}>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
