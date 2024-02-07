b90660a05e322f18cc2d339ae0410fd938344aef
Ingmar Glerum <ingmar.glerum@kvk.nl>
2024-02-07 11:26:53 +0100

    add placeholder for image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import imagePlaceholder from "../assets/no-image-placeholder-6f3882e0.webp";
+
 export const getCroppedImageUrl = (originalImageSource: string) => {
-  if (!originalImageSource) return "";
+  if (!originalImageSource) return imagePlaceholder;
   const cropAddition = "crop/600/400/";
   return (
     originalImageSource.slice(0, 28) +
